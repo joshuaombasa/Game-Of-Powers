1acdefabafbf671a2711ecb49932d032c4f614df
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-08 00:56:10 +0300

    Added initial script file

---
+++ index.js
@@ -0,0 +1,21 @@
+import {charactersData} from './data.js'
+
+import Character from './character.js'
+
+function render() {
+    document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
+    document.getElementById("monster").innerHTML = orc.getCharacterHtml()
+}
+
+const wizard = new Character(charactersData.hero)
+
+
+const orc = new Character(charactersData.monster)
+
+
+render()
+
+
+
+
+
