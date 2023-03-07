8ff1a89236e8b83519954dbff51113a3a803db26
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-08 00:54:25 +0300

    Added initial characters function

---
+++ character.js
@@ -0,0 +1,30 @@
+function Character(data) {
+    Object.assign(this, data) 
+
+    this.getRandomNumberArrayHtml = function () {
+        let randomArray = ''
+        return randomArray = new Array(this.diceCount).fill(0).map((num) => {
+        return `<div class="dice">${Math.floor(Math.random() * 6)+1}</div>`
+    })
+    }
+
+    this.getCharacterHtml = function () {
+        const {name, avatar, health, diceCount} = this;
+
+    let characterHtml = ''
+     return characterHtml = 
+
+    `<h3 class="character-name">${name}</h3>
+    <div class="img-container">
+    <img src="${avatar}" alt="" class="character-img">
+    </div>
+    <div class="character-health">health: <b>${health}</b> </div>
+    <div class="dice-container" id="dice-container">
+    ${this.getRandomNumberArrayHtml(this.diceCount).join('')}
+    </div>
+    `
+    }
+
+}
+
+export default Character
