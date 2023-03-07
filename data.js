4caa173fff7fd5d45987ca77041de25756b9011b
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-08 00:54:47 +0300

    Added initial data file

---
+++ data.js
@@ -0,0 +1,17 @@
+const charactersData = {
+    hero : {
+       name : "Wizard",
+       avatar:"images/wizard.png",
+       health:60 , 
+       diceCount: 3
+   },
+   
+    monster : {
+       name : "Orc",
+       avatar:"images/orc.png",
+       health:60,
+       diceCount: 1
+   }
+}
+
+export {charactersData}
